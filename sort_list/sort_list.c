20d424093173339ef3beb68d8c0336f230924684
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2024-09-11 16:23:10 +0200

    Add files via upload

---
+++ sort_list/sort_list.c
@@ -0,0 +1,25 @@
+#include "ft_list.h"
+
+
+t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
+{	
+	int temp;
+	t_list *head;
+
+	head = lst;//initialize head
+	
+	while (lst && lst->next)
+	{
+		// Apply the given function with *cmp to current and next node, where 0 means not sorted
+		if ((*cmp)(lst->data, lst->next->data) == 0)//if not sorted
+		{
+			temp = lst->data;//swap
+			lst->data = lst->next->data;
+			lst->next->data = temp;
+			lst = head;//reset lst to head to check from the beginning again
+		}
+		else //if sorted, we move to the next node
+			lst = lst->next;
+	}
+	return (head);
+}
