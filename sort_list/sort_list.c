73ec28a573aa500fa3141c1c6299e59ccb505aac
saraxplorer <147872964+saraxplorer@users.noreply.github.com>
2024-09-13 08:55:16 +0200

    Update sort_list.c

--- sort_list/sort_list.c
+++ sort_list/sort_list.c
@@ -4,7 +4,7 @@
 t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 {	
 	int temp;
-	t_list *head;
+	t_list *copy;
 
 	head = lst;//initialize head
 	
@@ -16,10 +16,10 @@ t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
 			temp = lst->data;//swap
 			lst->data = lst->next->data;
 			lst->next->data = temp;
-			lst = head;//reset lst to head to check from the beginning again
+			lst = copy;//reset lst to head to check from the beginning again
 		}
 		else //if sorted, we move to the next node
 			lst = lst->next;
 	}
-	return (head);
+	return (copy);
 }
